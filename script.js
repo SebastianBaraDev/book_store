4ef027acd15b90f4e71b360fff2f54f1719c6a27
Sebastian Bara <sebastianbara24@gmail.com>
2026-02-09 13:57:14 +0100

    added book Array with Objects and a render books function

--- script.js
+++ script.js
@@ -1,3 +1,138 @@
+let books = [
+  {
+    "name": "Die Krone von Eldoria",
+    "author": "Arin Falkenstein",
+    "likes": 2100,
+    "liked": true,
+    "price": 24.99,
+    "publishedYear": 2014,
+    "genre": "Fantasy",
+    "comments": [
+      {
+        "name": "FantasyHero",
+        "comment": "Eine epische Reise mit einer unglaublich detailreichen Welt."
+      },
+      {
+        "name": "LeserMax",
+        "comment": "Erinnert an klassische High-Fantasy – absolut großartig."
+      }
+    ]
+  },
+  {
+    "name": "Sterne über Andromeda",
+    "author": "Lena Kosmos",
+    "likes": 1650,
+    "liked": false,
+    "price": 19.50,
+    "publishedYear": 2020,
+    "genre": "Science-Fiction",
+    "comments": [
+      {
+        "name": "SciFiFan",
+        "comment": "Spannende Zukunftsvisionen und starke Charaktere."
+      }
+    ]
+  },
+  {
+    "name": "Der Pfad der inneren Klarheit",
+    "author": "Jonas Licht",
+    "likes": 3200,
+    "liked": true,
+    "price": 18.90,
+    "publishedYear": 2018,
+    "genre": "Persönlichkeitsentwicklung",
+    "comments": [
+      {
+        "name": "MindsetQueen",
+        "comment": "Ein inspirierendes Buch, das zum Nachdenken anregt."
+      },
+      {
+        "name": "LifeChanger",
+        "comment": "Hat meine Sicht auf viele Dinge nachhaltig verändert."
+      }
+    ]
+  },
+  {
+    "name": "Reichtum beginnt im Kopf",
+    "author": "Martin Goldmann",
+    "likes": 4100,
+    "liked": true,
+    "price": 21.00,
+    "publishedYear": 2016,
+    "genre": "Finanzen",
+    "comments": [
+      {
+        "name": "FinanceGuy",
+        "comment": "Einfach erklärt und extrem motivierend."
+      }
+    ]
+  },
+  {
+    "name": "Das Vermächtnis der Drachen",
+    "author": "Helena Sturm",
+    "likes": 2750,
+    "liked": false,
+    "price": 22.50,
+    "publishedYear": 2019,
+    "genre": "Fantasy",
+    "comments": []
+  },
+  {
+    "name": "Jenseits der Galaxiengrenze",
+    "author": "Tarek Nova",
+    "likes": 1980,
+    "liked": true,
+    "price": 20.00,
+    "publishedYear": 2017,
+    "genre": "Science-Fiction",
+    "comments": [
+      {
+        "name": "SpaceTraveler",
+        "comment": "Ein klassisches Weltraumabenteuer mit modernem Touch."
+      }
+    ]
+  },
+  {
+    "name": "Der Alchemist des Alltags",
+    "author": "Samuel Rivera",
+    "likes": 3600,
+    "liked": true,
+    "price": 17.99,
+    "publishedYear": 2015,
+    "genre": "Persönlichkeitsentwicklung",
+    "comments": [
+      {
+        "name": "SoulReader",
+        "comment": "Poetisch, tiefgründig und sehr inspirierend."
+      }
+    ]
+  },
+  {
+    "name": "Finanzielle Freiheit Schritt für Schritt",
+    "author": "Robert Stein",
+    "likes": 2900,
+    "liked": false,
+    "price": 23.75,
+    "publishedYear": 2021,
+    "genre": "Finanzen",
+    "comments": [
+      {
+        "name": "MoneyMind",
+        "comment": "Praxisnah und gut strukturiert – ideal für Einsteiger."
+      }
+    ]
+  }
+];
+
+function renderBooks() {
+  let bookList = document.getElementById("book_list");
+  bookList.innerHTML = "";
+
+    for (let bookIndex = 0; bookIndex < books.length; bookIndex++) {
+        bookList.innerHTML += getBookTemplate(bookIndex);
+    }
+}
+
 let bookList = document.getElementById("book_list");
 
 // Buchtitel like / dislike
--- scripts/template.js
+++ scripts/template.js
@@ -1,3 +1,35 @@
 // Book List Template
+function getBookTemplate(bookIndex) {
+    return `<div class="BookWidget">
+    <h3>${allBooks.booksTitle[bookIndex]}</h3>
+    <p>${allBooks.booksDescription[bookIndex]}</p>
+    <div class="icon-container">
+    <img class="icons" src="./assets/icons/trash_icon.png" onclick="moveBook(${bookIndex}, 'books', 'trashBooks')">
+    <img class="icons" src="./assets/icons/archiv_icon.png" onclick="moveBook(${bookIndex}, 'books', 'archivBooks')">
+    </div>
+    </div>`;
+}
+
+//Book Title Box
+//Book Cover Box
+//Book Price Box
+    //Price
+        //Likes
+        //img
+//Book Rating Box
+    //Five empty stars
+//Book Description Box
+    //Author
+    //Genre
+    //Release Date
+//Comment Section Box
+    //Comment Box
+        //Titel
+        //Username
+        //Comment Text
+    //Comment Input Box
+        //Input Field
+        //Submit Button img
+
 
 // Add Book
